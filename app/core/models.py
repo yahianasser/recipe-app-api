ab8306b5f0c032b12dd8b740eee1ec96d7fb47d4
yahianasser <yahia.nasser2001@gmail.com>
2025-06-30 22:17:19 +0300

    changes

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
+import string
+import random
 
-# Create your models here.
+class UrlManager(models.Manager):
+  def create_url(self, original_url):
+    shortened_code = self.generate_unique_code()
+    if not original_url or original_url == '':
+      raise ValueError('a Url must be entered.')
+    return self.create(original_url=original_url, shortened_code=shortened_code)
+
+  def generate_unique_code(self, length=6):
+    characters = string.ascii_letters + string.digits
+    while True:
+      code = ''.join(random.choices(characters, k=length))
+      if not self.filter(shortened_code=code).exists():
+        return code
+
+class Url(models.Model):
+    url_id = models.AutoField(primary_key=True)
+    original_url = models.URLField(max_length=255, unique=True)
+    shortened_code = models.CharField(max_length=20, unique=True)
+    clicks = models.IntegerField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    last_accessed = models.DateTimeField(null=True, blank=True)
+
+    objects = UrlManager()
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,12 @@
+# from django.test import TestCase
+# from core import models
+
+# class ModelTests(TestCase):
+#   def test_create_url_successful(self):
+#     url = 'url'
+#     new_url = url_model.create_url(url=url)
+#     self.assertEqual(new_url, url)
+
+#   def test_new_url_empty(self):
+#     with self.assertRaises(ValueError):
+#       models.UrlManager.create_url(original_url='')
